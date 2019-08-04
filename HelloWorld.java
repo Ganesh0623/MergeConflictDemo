62c5d0e37efed5e6b611abcaaa9e4b981b168e0c
ganesh <b@gmail.com>
2019-08-04 17:45:33 -0500

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,14 @@
 public class A
 {
+<<<<<<< HEAD
     public static void add()
     {
 
 System.out.println("Add Method");
 
     }
+=======
+>>>>>>> parent of cfeb1c9... by Yash
 
 public static void main (String [] args)
 {
@@ -15,8 +18,11 @@ System.out.println("Hello World");
  System.out.println("Line 8 from Remote Repos");
  
 System.out.println("Hello World");
+<<<<<<< HEAD
     add();
     
+=======
+>>>>>>> parent of cfeb1c9... by Yash
 
 
 }
